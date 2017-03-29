1b6fe223354bff87a0fd575b7a2cbb9693035137
Tylor Bayer <tylor.bayer@gmail.com>
2017-03-29 16:26:37 -0600

    Riley updates

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         com = data[comment];
         everything += "<iframe src=" + com.Name + "></iframe><br>";
       }
-      $("#comments").html(everything);
+      $("#our_comments").html(everything);
     })
   });
 

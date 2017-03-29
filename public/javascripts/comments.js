9fb30efedd0a2e31c52b727457f81f5dfbeac284
Tylor Bayer <tylor.bayer@gmail.com>
2017-03-29 15:37:53 -0600

    real first commit

---
+++ public/javascripts/comments.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $("#postComment").click(function(){
+      var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
+      jobj = JSON.stringify(myobj);
+      $("#json").text(jobj);
+
+      var url = "comment";
+      $.ajax({
+      url:url,
+      type: "POST",
+      data: jobj,
+      contentType: "application/json; charset=utf-8",
+      success: function(data,textStatus) {
+          $("#done").html(textStatus);
+      }
+    })
+  });
+
+  $("#getComments").click(function() {
+    $.getJSON('comment', function(data) {
+      console.log(data);
+      var everything = "<ul>";
+      for(var comment in data) {
+        com = data[comment];
+        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+      }
+      everything += "</ul>";
+      $("#comments").html(everything);
+    })
+  })
+
+$("#deleteComments").click(function() {
+  console.log('delete buttom clicked');
+  var url = "comment";
+  $.ajax({
+    url:url,
+    type: "DELETE",
+    success: function(data,textStatus) {
+    }
+  })
+});
+});
